231c1bdcd2ae41157fba24d004d639f0a85ec53f
Valentin <5619@holbertonstudents.com>
2022-10-03 14:25:07 -0700

    4.alphabets without q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
 * main - Entry point
 *
@@ -11,13 +10,9 @@
 int main(void)
 {
 char ch;
-        {
-        for (ch = 'a'; ch <= 'z'; ch++)       
-                if (ch != 'e' && ch != 'q')
-                { 
-                putchar(ch);
-                }
-                putchar('\n');
+        for (ch = 'a'; ch <= 'z'; ch++)          
+        if (ch != 'e' && ch != 'q')
+        putchar(ch);
+        putchar('\n');
         return (0);
-        }
 }
