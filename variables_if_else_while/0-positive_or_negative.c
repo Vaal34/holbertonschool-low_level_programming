ee12134d1451a8715ccae5a734e9ca9fb581128e
Valentin <5619@holbertonstudents.com>
2022-10-03 03:23:46 -0700

    negative or positive

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
 
 /* betty style doc for function main goes there */
 int main(void)
