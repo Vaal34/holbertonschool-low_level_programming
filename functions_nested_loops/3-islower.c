68f861616934adb0a9cf773dad64c7f120e37489
Valentin <5619@holbertonstudents.com>
2022-10-05 12:00:07 -0700

    4.islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* 3-islower - check the code
+*
+* Return: On success 1.
+*
+* Description:  A column of asterisks on the left side
+*, with beginning and ending almost-blank lines.
+*/
+
+int _islower(int c);
+
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
