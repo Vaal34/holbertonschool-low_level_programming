adb2af80076b49f304b857e58e953991e9c36ae2
Valentin <5619@holbertonstudents.com>
2022-10-05 05:53:58 -0700

    alphabet with _putchar

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,8 +4,7 @@
 *
 * Return: Always 0.
 *
-* Description:  A column of asterisks on the left side,
-* with beginning and ending almost-blank lines.
+* Description:  A column of asterisks on the left side, with beginning and ending almost-blank lines.
 */
 
 void print_alphabet(void)
