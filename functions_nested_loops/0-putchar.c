790ce5bfd5c8f003d3e1150459a46a248d3ba17c
Valentin <5619@holbertonstudents.com>
2022-10-05 01:04:06 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*
+* Description:  A column of asterisks on the left side,
+* with beginning and ending almost-blank lines.
+*/
+
+int main (void)
+{
+
+puts("_putchar");
+
+return (0);
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_MAIN
+#define HEADER_MAIN
+
+int main(void);
+
+#endif
+
