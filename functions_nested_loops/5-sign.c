861b2875554a62131f1225cc43fd3e79ec48c5ba
Valentin <5619@holbertonstudents.com>
2022-10-05 13:31:25 -0700

    5.sign if, else if, else

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,20 +5,19 @@
 * Return: 1 if c is lowercase, 0 otherwise
 */
 int print_sign(int n)
-/* prototype for the function */
 {
 	if (n > 0)
-	{	
+	{
 		_putchar('+');
 		return (1);
-	}	
-	else if(n == 0)
-	{	
+	}
+	else if (n == 0)
+	{
 		_putchar('0');
 		return (0);
 	}
 	else
-	{	
+	{
 		_putchar('-');
 		return (-1);
 	}
