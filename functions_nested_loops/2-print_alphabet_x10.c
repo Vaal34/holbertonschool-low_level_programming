a8ef4ee41a2206db51ecac6c446a1a1136eef594
Valentin <5619@holbertonstudents.com>
2022-10-05 08:00:41 -0700

    2. alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,16 @@
 */
 
 void print_alphabet_x10(void)
-{	
+{
 	int n;
 	char ch;
+
 	for (n = 0; n <= 9; n++)
-		{
+	{
 		for (ch = 'a'; ch <= 'z'; ch++)
-	 		{
+		{
 			_putchar(ch);
-			}
-		_putchar('\n');
 		}
+		_putchar('\n');
+	}
 }
