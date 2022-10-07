492b68f904c9c7fe6b08f389e2d5e5bc965a0569
Valentin <5619@holbertonstudents.com>
2022-10-07 13:59:09 -0700

    6.print _

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * print_line - print a straight line
+  * @n: to check
+  */
+void print_line(int n)
+{
+int r; /* nombre of loop turns */
+char t = '_';
+
+for (r = 0; r < n; r++)
+{
+if (n <= 0)
+{
+}
+else
+{
+_putchar(t);
+}
+}
+_putchar('\n');
+} 
