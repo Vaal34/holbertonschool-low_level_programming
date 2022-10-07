438ca37f401802d331de0d04db9c8dc7bcbcabfe
Valentin <5619@holbertonstudents.com>
2022-10-07 10:58:46 -0700

    10.additions

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
 /**
- * add - adds two intiger
- * return: Always 0.
- **/
+* add - adds two intiger
+*
+*return: Always 0.
+*
+*@a= intiger
+*@b= intiger
+**/
 
 int add(int a, int b)
 {
