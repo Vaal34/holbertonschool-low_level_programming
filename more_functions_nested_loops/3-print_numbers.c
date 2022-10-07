9f3108006c0d1ccbfd75e3d57bee8a8bbd2faa4d
Valentin <5619@holbertonstudents.com>
2022-10-07 02:27:19 -0700

    3.numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*mul - check the code
+*@b: the character to check
+*@a: the character to check
+*Return: the multiply of a*b
+*/
+
+void print_numbers(void)
+{
+int n = '0';	
+	for (n = '0'; n <= '9'; n++)
+	{
+	_putchar(n);
+	}
+	_putchar('\n');
+}
