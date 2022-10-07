2446e72788363d9cc5aa08f81f674d43e9b0f3c9
Valentin <5619@holbertonstudents.com>
2022-10-07 03:08:31 -0700

    4.Number without 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* print_numbers - print numbers
+* print_most_numbers - print_most_numbers
 **/
 
 void print_most_numbers(void)
 {
 int n = 0;
 
-for (n = 0; n <= 9; n++)	
+for (n = 0; n <= 9; n++)
 	if (n != 2 && n != 4)
 		_putchar(n + '0');
 _putchar('\n');
