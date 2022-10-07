e1899afee2094664929bb0116bbf99f1559b6fec
Valentin <5619@holbertonstudents.com>
2022-10-07 13:43:33 -0700

    5.10 to 14x10

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers
+**/
+
+void more_numbers(void)
+{
+int n = 0;
+int r = 0;
+
+for (r = 0; r <= 10; r++)
+{
+	for (n = 0; n <= 14; n++)
+	{
+		if (n >= 10)
+	{
+		_putchar((n / 10) + '0');
+	}
+		_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
+}
+}
