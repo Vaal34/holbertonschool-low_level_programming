92be245686eff4218ae2faf08228a317e5a789a8
Valentin <5619@holbertonstudents.com>
2022-10-08 14:59:33 -0700

    7.print diago

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,29 @@
 #include "main.h"
 /**
- * print_line - print a straight line
+ * print_diagonal - draws a diagonal line on the terminal
  * @n: to check
  */
 void print_diagonal(int n)
 {
-	int t = 0;
-	int u = 0;
-	char ch = '\\';
-	char esp = '.';
-
+	int r = 0; /* nbr of loop turns */
+	int l = 0;
+	int s = '\\';
+	int e =  ' ';
 
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	for (t = 0; t < n; t++)
-	{
-		while (u > n)
+		for (r = 0; r < n; r++)
 		{
-			t = u;
-			_putchar(esp);
-			_putchar(ch);
+			l = r;
+			while (l > 0)
+			{
+				_putchar(e);
+				l--;
+			}
+
+			_putchar(s);
+			_putchar('\n');
 		}
-		t++;
-		_putchar('\n');
-	}
 }
--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,18 +2,30 @@
 #include <stdio.h>
 
 /**
-* main - it's the entry point
-* Return - 0 if is true
-**/
+ * main - it's the entry point
+ * Return - 0 if is true
+ **/
 
 int main(void)
 {
-int n;
+	int n;
 
-for (n = 0; n <= 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
-	printf("%d ", n);
+		if ((n % 3) == 0 && (n % 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((n % 3) == 0)
+			printf("Fizz");
+
+		else if ((n % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", n);
 	}
-	putchar('\n');
+
+	printf("\n");
+
 	return (0);
 }
