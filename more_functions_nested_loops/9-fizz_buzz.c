9a40dcabd0da7bc7715dae4f384945e10e66ab58
Valentin <5619@holbertonstudents.com>
2022-10-08 14:40:48 -0700

    9.Fizz Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* main - it's the entry point
+**/
+
 int main(void)
 {
 int n;
@@ -10,5 +14,5 @@ for (n = 0; n <= 100; n++)
 	printf("%d ", n);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
