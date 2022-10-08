08966710e58e607e32beafcc53ad0ea1f44b463c
Valentin <5619@holbertonstudents.com>
2022-10-08 11:27:35 -0700

    8.print the square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+void print_square(int size)
+{
+
+	int r = 0;
+	int l = 0;
+	char ch = '#';
+		
+		if (size <= 0)
+		{
+			_putchar('\n');
+		}
+		for (l = 0; l <= size; l++)
+		{	
+			for (r = 0; r <= size; r++)
+			{
+				_putchar(ch);
+			}
+			_putchar('\n');
+			}
+}
+
