91d6b0ee12c0d94a20feabb41673c3fcc1b2df72
Valentin <5619@holbertonstudents.com>
2022-10-10 06:33:39 -0700

    0. 402 to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* reset_to_98 - updates the value it points to to 98. 
+* reset_to_98 - updates the value it points to to 98.
 *
+*@n - is the variable check
 */
 
 void reset_to_98(int *n)
