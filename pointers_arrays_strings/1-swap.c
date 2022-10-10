d45ddb16e4c4f7756b37c18b626b023905f5925e
Valentin <5619@holbertonstudents.com>
2022-10-10 06:58:28 -0700

    1.Swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-* reset_to_98 - updates the value it points to to 98.
+* swap_int - swaps the values of two integers
 *
 *@a: is the variable check
 *@b: is the variable check
+*
 */
 
 void swap_int(int *a, int *b)
