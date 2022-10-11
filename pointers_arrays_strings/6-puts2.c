05032bc12e176bc2e3dcb32e26913a38108eb40e
Valentin <5619@holbertonstudents.com>
2022-10-11 11:47:11 -0700

    6.one digit on two

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts2 - print the string one on two
+ *
+ *@str: the string
+ *
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
