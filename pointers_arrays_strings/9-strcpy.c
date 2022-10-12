b772c78d642ec1991b9c7eb0ebc17bc71d9685ba
Valentin <5619@holbertonstudents.com>
2022-10-12 10:23:53 -0700

    9. copy the pointers

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *_strcpy - copy src in dest
+ * _strcpy - copy src in dest
  *
  *@dest: the destination²:wq
- *@src: the pointers of the string 
+ *@src: the pointers of the string
  */
 
 char *_strcpy(char *dest, char *src)
