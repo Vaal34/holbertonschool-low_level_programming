4256fb9752520e337108c134171bdd261680dbd9
Valentin <5619@holbertonstudents.com>
2022-10-12 00:38:51 -0700

    6.one line on two

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,14 @@
 void puts2(char *str)
 {
 	while (*str != 0)
-	{
-		_putchar(*str);
-		str++;
-		str++;
+	{	
+		if ((*str % 2) == 0)
+		{
+		}
+		{
+			_putchar(*str);
+			str+= 2;
+		}
 	}
 	_putchar('\n');
 }
