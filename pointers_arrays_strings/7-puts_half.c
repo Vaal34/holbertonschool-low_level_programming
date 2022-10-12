ea886061043ac887f50117907839dc38ba151db5
Valentin <5619@holbertonstudents.com>
2022-10-12 03:10:30 -0700

    7. print the second part of the string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,15 +12,13 @@ void puts_half(char *str)
 	int c = 0;
 	int i = 0;
 	
-	while (str[i] != 0)
-	{
-		i++; /* number of array */
-		c++; /* number of loops */
+	while (str[i++])
+	{/* number of array */
+		c++;/* number of loops */
 	}
 		i = ((c - 1) / 2);
-	while (str[i] != 0)
+	while (str[i++])
 	{
-		i++;
 		_putchar(str[i]);
 	}
 	_putchar('\n');
