d035cccfebb17ee80bc61ca986c6724fc1cfad10
Valentin <5619@holbertonstudents.com>
2022-10-13 05:58:28 -0700

    0.concatenates two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+
+	int i;
+
+	for (i = 0; i != 0; i++)
+	{
+		dest++;
+	}
+	for (i = i - 1; i != 0; i++)
+	{
+		src++;
+	}
+	return (0);
+}
