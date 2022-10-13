3d403defd08625c2d1ece6e3031f3876b388dac7
Valentin <5619@holbertonstudents.com>
2022-10-13 14:10:44 -0700

    4.2.reverse an array int

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+ * reverse_array - reverse an array of integer
+ *@a: an array of integers
+ *@n: the number of elements to swap
+ *
+ */
+
 void reverse_array(int *a, int n)
 {
 	int i = 0;
-	int tmp = 0;	
+	int tmp = 0;
 	int g = 0;
 
 	for (i = n - 1; i >= g; i--)
