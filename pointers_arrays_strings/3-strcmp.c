a42e4748643662558b8af13c64b10ff8268f7af9
Valentin <5619@holbertonstudents.com>
2022-10-14 01:14:18 -0700

    3.2.Compare two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
+/**
+ * _strcmp - compare two strings
+ * @s1: first a string
+ * @s2: second string
+ * Return: Always return 0
+ */
 int _strcmp(char *s1, char *s2)
 {
-	return s1[0] - s2[0];
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[0] - s2[0]);
+	}
+	return (0);
 }
