84d7b7875f6af89c63cd5ca1532487cc30b486b3
Valentin <5619@holbertonstudents.com>
2022-10-14 09:41:27 -0700

    7.2.encode a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * leet - encodes a string into 1337.
+  *@c: pointer
+  *Return : the string
+  */
+
+char *leet(char *c)
+{
+	int i, y;
+
+	int n[] = {4, 3, 0, 7, 1};
+	int r[] = {"aAeEoOtTlL"};
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (c[i] == r[i])
+			{
+				c[i] = n[y / 2];
+				break;
+			}
+
+
+		}
+	}
+	return (c);
+}
