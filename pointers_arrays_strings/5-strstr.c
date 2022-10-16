5d156d39042320727bded1398f18d122b3be51c8
Valentin <5619@holbertonstudents.com>
2022-10-16 10:19:12 -0700

    7.3. Chessboard with two arrays

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int c;
+
+	for (c =0; *haystack != '\0'; c++)
+	{
+	}
+	for (i = 0; *needle != '\0'; i++)	
+	{
+	}
+	return (haystack);
+}
