3bd0148c051231797203b253acb35ae98b719451
Valentin <5619@holbertonstudents.com>
2022-10-17 08:21:50 -0700

    3.Factorial of n

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,9 +8,14 @@
 int factorial(int n)
 {
 	if (n < 0)
-		{
-			return (-1);
-		}
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
 	else
-		return factorial(n) = factorial(factorial - 1);
+		return (n * factorial(n - 1));
 }
+
