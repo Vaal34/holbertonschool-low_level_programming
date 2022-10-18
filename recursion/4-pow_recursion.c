e73e830938d62b26fd2c60fc9bc94648c151fe00
Valentin <5619@holbertonstudents.com>
2022-10-18 00:23:44 -0700

    4.x power y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -10,8 +10,14 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
+		return (-1);
+	}
+	if (y != 0)
+	{
+		return (_pow_recursion(x, y - 1) * x);
+	}
+	else 
+	{
 		return (1);
 	}
-	else
-		return (_pow_recursion(x, y - 1) * x);
 }
