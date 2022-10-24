6007786ed4d1c71ab3b15441beed11b57b3b0f83
Valentin <5619@holbertonstudents.com>
2022-10-24 08:58:30 -0700

    1.Duplicated

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char
+ *@size: size of arrays
+ *@c: a character
+ *Return: Always 0
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int size = 0;
+	char *p;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		size++;
+	}
+	p = malloc((sizeof(char) * size) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		p[i] = str[i];
+	}
+	p[i] = '\0';
+	return (p);
+}
