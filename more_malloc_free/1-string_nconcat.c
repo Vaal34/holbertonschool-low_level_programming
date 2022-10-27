1ce48e3e248555def1c79bbda7e3c0352b57f34e
Valentin <5619@holbertonstudents.com>
2022-10-27 06:33:01 -0700

    1.concat n

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int x, a, size;
 	unsigned int b, y;
 	char *p;
-
+	
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{ 
+		s2 = "";
+	}
 	for (a = 0; s1[a] != '\0'; a++)
 	{
 	}
