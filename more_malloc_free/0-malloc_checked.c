d8373d144aa4bf557a600859a5428691e8e7d502
Valentin <5619@holbertonstudents.com>
2022-10-27 02:26:21 -0700

    0. allocates memory using malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
+
+	p = malloc(sizeof(unsigned int) * b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
