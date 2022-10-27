ac1cca4349fd110bcf613f3f62e743ef5c478535
Valentin <5619@holbertonstudents.com>
2022-10-27 02:57:59 -0700

    2.concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,18 +1,47 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - is the main
- * @argc: arguments count
- * @argv: arguments value
- * Return: Always return 0
+ * str_concat - concat 2 strings in a new pointers
+ *@s1: string 1
+ *@s2: string 2
+ *Return: a pointers
  */
 
-int main(int argc, char *argv[])
+char *str_concat(char *s1, char *s2)
 {
-	int c = 0;
+	int x, y, a, b, size;
+	char *p;
 
-	for (c = 0; c < argc; c++)
-		printf("%s\n", argv[c]);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+	}
+	size = a + b;
 
-	return (0);
+	p = malloc(sizeof(char) * size + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < a; x++)
+	{
+		p[x] = s1[x];
+	}
+	for (y = 0; y < b; y++)
+	{
+		p[x + y] = s2[y];
+	}
+	p[x + y] = '\0';
+	return (p);
 }
