a3b9a70c1d84da35c726b314c58ebdf50be5625e
Valentin <5619@holbertonstudents.com>
2022-10-28 06:06:43 -0700

    3.array min to max

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -17,10 +17,10 @@ int *array_range(int min, int max)
 		return (NULL);
 
 
-	p = malloc(sizeof(int) * max);
+	p = malloc(sizeof(int) * (max - min + 1));
 		if (p == 0)
 			return (NULL);
-		for (i = 0; i < max; i++)
+		for (i = 0; i < max + 1; i++)
 		{
 		p[i] = i;
 		}
