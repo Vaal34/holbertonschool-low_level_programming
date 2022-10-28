939fe6eb4297a7c0f1cbe14843b8171cf463076f
Valentin <5619@holbertonstudents.com>
2022-10-28 05:57:07 -0700

    3.array min to max

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -4,25 +4,25 @@
 /**
  * array_range -  function that allocates memory for an array, using malloc
  * @min: min
- *
+ * @max: max
+ * Return: the new pointers
+ */
 
 int *array_range(int min, int max)
 {
-	char *p;
-	unsigned int i;
+	int *p;
+	int i;
 
-	if (nmemb == 0 || size == 0)
-	{
-		return (0);
-	}
-	p = malloc(nmemb * size);
-	if (p == 0)
-	{
-		return (0);
-	}
-	for (i = 0; i != nmemb * size; i++)
-	{
-		p[i] = 0;
-	}
-	return (p);
+	if (min > max)
+		return (NULL);
+
+
+	p = malloc(sizeof(int) * max);
+		if (p == 0)
+			return (NULL);
+		for (i = 0; i < max; i++)
+		{
+		p[i] = i;
+		}
+		return (p);
 }
