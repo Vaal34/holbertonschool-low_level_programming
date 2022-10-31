0e0729822e0c2c6c38e57e508574ae78589699d1
Valentin <5619@holbertonstudents.com>
2022-10-31 16:07:49 -0700

    0.dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOGS
+#define DOGS
+
+#include <stdio.h>
+
+/**
+ * struct dog - print the informations of poppy
+ *@name: is his name
+ *@age: his age
+ *@owner: my owner
+ *
+ * Description: Dexcribe all information on the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
