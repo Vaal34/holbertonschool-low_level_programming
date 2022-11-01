f680fd1428461e31f2ed8705b0e0ef62b69e5bf9
Valentin <5619@holbertonstudents.com>
2022-11-01 03:43:44 -0700

    2.print ifo dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+		printf("Age: %f\n", (*d).age);
+		printf("Owner: %s\n", (*d).owner);
+	}
+	else if ((d->name) || (d->age) || (d->owner) == NULL)
+	{
+		printf("Name: (nil)\n");
+		printf("Age: (nil)\n");
+		printf("Owner: (nil)\n");
+	}
+}
+
