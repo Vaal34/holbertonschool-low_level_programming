e5bdea6ea5a51f33bca9c899c3bcceb725d10561
Valentin <5619@holbertonstudents.com>
2022-11-01 08:08:06 -0700

    5.free dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,10 +1,17 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * free_dog - free malloc
+ * @d: structur
+ */
+
 void free_dog(dog_t *d)
 {
 	free((*d).name);
+	(*d).name = NULL;
 	free((*d).owner);
+	(*d).owner = NULL;
 	free(d);
 	d = NULL;
 }
