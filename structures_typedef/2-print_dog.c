13588447e5b87ce805939594147f5291cd3892da
Valentin <5619@holbertonstudents.com>
2022-11-01 04:33:11 -0700

    2.print ifo dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,17 +2,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * print_dog - prints the dog struct
+ * @d: pointer dog struct
+ */
+
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		exit(0);
-	if ((*d).name == NULL)
+	if ((*d).name == NULL ||(*d).owner == NULL)
+	{
 		printf("Name: (nil)\n");
+		printf("Age: %f\n", (*d).age);
+		printf("Owner (nil)\n");
+	}
 	else
+	{
 		printf("Name: %s\n", (*d).name);
-	printf("Age: %f\n", (*d).age);
-	if ((*d).owner == NULL)
-		printf("Owner: (nil)\n");
-	else
+		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
+	}
 }
