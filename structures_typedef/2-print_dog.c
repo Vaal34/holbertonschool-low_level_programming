985699189431818ab54254a4d8212bc1fbb1c1e0
Valentin <5619@holbertonstudents.com>
2022-11-01 03:56:48 -0700

    2.print ifo dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,18 +4,16 @@
 
 void print_dog(struct dog *d)
 {
-	char *f = "(nil)";
-
+	if ((*d).name == NULL || (*d).owner == NULL)
+	{
+		d->name = "(nil)";
+		d->owner = "(nil)";
+	}
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (*d).name);
 		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
-	}
-	else if (d->name == NULL || d->owner == NULL)
-	{
-		printf("Name: %s\n", f);
-		printf("Owner: %s\n", f);
 	}
 }
 
