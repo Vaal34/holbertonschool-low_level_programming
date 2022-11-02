aebdcf95b917d0f9d319e8bf9cdaa79a998e8e36
Valentin <5619@holbertonstudents.com>
2022-11-02 05:56:15 -0700

    0.print_name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: string name
+ * @f: function pointers
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	int i;
+
+	if (name != NULL)
+	{
+		for (i = 0; name[i] != '\0'; i--)
+			f(&name[i]);
+	}
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,26 @@
+#ifndef POINTERS
+#define POINTERS
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
+
+#endif
