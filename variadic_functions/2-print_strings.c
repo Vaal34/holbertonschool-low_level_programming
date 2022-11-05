88f60895a7cdb6ce8a2485833201046515cd4199
Valentin <5619@holbertonstudents.com>
2022-11-05 10:49:24 -0700

    2.print strings with stdarg

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", print);
 		if (separator == NULL)
 		{
-		printf("(nil)");
+		else if (n == NULL)
+		{
+			printf("(nil)");
 		}
 		else if (i < n - 1)
 		{
