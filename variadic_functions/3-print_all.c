07d37f9bd4f5e1c86a080f7d7b546bbc429af1cb
Valentin <5619@holbertonstudents.com>
2022-11-11 04:38:08 -0800

    3.print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,83 @@
+#include "variadic_functions.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * print_c - print a char
+ * @args: args
+ */
+void print_c(va_list args)
+{
+	int a = va_arg(args, int);
+
+	write(1, &a, 1);
+}
+
+/**
+ * print_s - print string
+ * @args : args
+ */
+void print_s(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	write(1, str, _strlen(str));
+}
+
+/**
+ * print_i - print an integer
+ * @args: args
+ */
+void print_i(va_list args)
+{
+	int n = va_arg(args, int);
+
+	write(1, &n, 1);
+}
+
+/**
+ * print_fl - print float
+ * @args: args
+ */
+void print_fl(va_list args)
+{
+	double i = va_arg(args, double);
+
+	printf("%f", i);
+}
+
+/**
+ * print_all - print all flag
+ * @format: the string
+ */
+void print_all(const char * const format, ...)
+{
+	fp fandfunc[] = {
+		{'f', print_fl},
+		{'i', print_i},
+		{'s', print_s},
+		{'c', print_c},
+		{0, NULL}
+	};
+
+	va_list args;
+	int i = 0;
+	int b;
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		b = 0;
+		while (fandfunc[b].flag != 0)
+		{
+			if (fandfunc[b].flag == format[i])
+			{
+				fandfunc[b].pointer_function(args);
+			}
+			b++;
+		}
+		i++;
+	}
+	va_end(args);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,5 +9,11 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _strlen(char *s);
+
+typedef struct functionp{
+	char flag;
+	void (*pointer_function)(va_list);
+}fp;
 
 #endif
