069d28616d92eb04cf4d72cc72bf2814f19f98ba
Valentin <5619@holbertonstudents.com>
2022-11-16 06:54:19 -0800

    5.free listint 2

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free all list
+ * @head: the first node
+ */
+
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+		free(head);
+	else
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
