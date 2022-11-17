96cabf77bb3bb55b833fdb47035f76f5f5509e7a
Valentin <5619@holbertonstudents.com>
2022-11-17 00:38:30 -0800

    5. free list 2

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,12 @@
 void free_listint2(listint_t **head)
 {
 	if (*head == NULL)
-		free_listint2((*head)->next);
 		free(*head);
+	else
+	{
+		free_listint2((*head)->next);
+		free((*head)->n);
+		free(*head);
+		free(head);
 	}
 }
