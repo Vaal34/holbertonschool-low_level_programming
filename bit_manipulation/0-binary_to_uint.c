2dcdfdcc489d0cf7e489404cb8e9762f8a6f25c2
Valentin <5619@holbertonstudents.com>
2022-11-18 05:23:49 -0800

    0.binary to decimal

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - binary to decimal
+ * @b: 1 & 0
+ * Return: decimal
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int lewn;
+
+	len = strlen(b);
+
+	for (c = len - 1; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+		{
+			i += a;
+		}
+		a = a * 2;
+	}
+	return (i);
+}
