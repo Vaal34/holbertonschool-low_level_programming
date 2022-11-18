6b1952aa12638783fb107ef2796fa42d106c4dbe
Valentin <5619@holbertonstudents.com>
2022-11-18 02:14:37 -0800

    7.return nth node

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * get_nodeint_at_index - that returns the nth node of a listint_t linked list
+ * @head: pointer of struct, first node
+ * @index: number of node
+ * Return: the nth node
+ */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	:
+	listint_t *tmp;
+	unsigned int count = 0;
+
+	tmp = head;
+
+	while (tmp != NULL)
+	{
+		if (count == index)
+			return (head->next);
+		count++;
+		tmp = tmp->next;
+	}
+	return (head);
+}
