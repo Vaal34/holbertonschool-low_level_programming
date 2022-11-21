3f643c4ae12ba0bd04cddebf89dda0c478e4bd04
Valentin <5619@holbertonstudents.com>
2022-11-21 07:36:51 -0800

    0.reads a text file and return the lenght

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,16 +13,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buffer;
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY); /*open the file*/
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters); /* malloc lenght of str */
 	if (buffer == NULL)
 		return (0);
 
-	read(fd, buffer, letters);
+	read(fd, buffer, letters); /* remlpit le buffer */
 
 	close(fd);
-	return (write(STDOUT_FILENO, buffer, strlen(buffer)));
+	return (write(STDOUT_FILENO, buffer, strlen(buffer))); /* print the file */
 }
