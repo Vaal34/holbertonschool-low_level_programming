1299b6747cc7b8b43162d1013213b4170c0c0de0
Valentin <5619@holbertonstudents.com>
2022-11-22 02:02:06 -0800

    1.create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * create_file - Create a function that creates a file
+ * @filename: file create
+ * @text_content: text in the file
+ * Return: -1 if fail and 1 if run
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -9,8 +16,9 @@ int create_file(const char *filename, char *text_content)
 	if (!filename || fd == -1)
 		return (-1);
 
-	write(fd, text_content, strlen(text_content));
-	
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
+
 	close(fd);
 
 	return (1);
