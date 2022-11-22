dad25cd3f4c76c076ee9f82d45b86606f0ef568c
Valentin <5619@holbertonstudents.com>
2022-11-22 01:42:48 -0800

    1.create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,16 +5,20 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (filename == NULL)
-		return (-1);
+	if (filename && text_content != NULL)
+	{
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	text_content = malloc(sizeof(char) * strlen(text_content));
-	if (text_content == NULL)
-		return (0);
+		read(fd, text_content, strlen(text_content));
 
-	write(fd, text_content, strlen(text_content));
+		write(fd, text_content, strlen(text_content));		
 
-	close(fd);
-	return (1);
+		close(fd);
+		return (1);
+	}
+	else
+	{
+	fd = open(filename, O_CREAT, 0600);
+	}
+	return (-1);
 }
