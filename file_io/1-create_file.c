7fd62d1991507820caeceb69f35ec48dc1721a45
Valentin <5619@holbertonstudents.com>
2022-11-22 01:47:52 -0800

    1.create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,7 +5,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	if (filename && text_content != NULL)
+	if (filename != NULL && text_content != NULL)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-	fd = open(filename, O_CREAT, 0600);
+	fd = open(filename, O_CREAT, 0200);
 	}
 	return (-1);
 }
