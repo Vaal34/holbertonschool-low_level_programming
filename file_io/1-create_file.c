6241c36ceb7641037043cef9ad670a4e6b92e50a
Valentin <5619@holbertonstudents.com>
2022-11-22 00:58:00 -0800

    1.create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	text_content = malloc(sizeof(char) * strlen(text_content));
+	if (text_content == NULL)
+		return (write(fd, text_content, 0));
+
 	read(fd, text_content, strlen(text_content));
+
+	write(fd, text_content, strlen(text_content));
 
 	close(fd);
 	return (1);
