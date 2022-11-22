5dbdd2785e8d8795bb76410b1671e72f777298fb
Valentin <5619@holbertonstudents.com>
2022-11-22 08:45:20 -0800

    2. adds a new node at the beginning of the list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,11 +10,11 @@
  * Return: the address of the new element, or NULL if it failed
  */
 
-listint_t *add_dnodeint(listint_t **head, const int n)
+listint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	listint_t *p;
 
-	p = malloc(sizeof(listint_t));
+	p = malloc(sizeof(dlistint_t));
 	if (p == NULL)
 	{
 		return (NULL);
