63dff2e55fb99055694a63752b8ad5fe70fccc3f
Valentin <5619@holbertonstudents.com>
2022-11-23 05:25:46 -0800

    3.add node addd the end of a double ll

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *add_dnodeint_end - adds a new node at the end of a listint_t list
+ * @head: first node
+ * @n: element of the struct
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *p, *tmp;
+
+	p = malloc(sizeof(dlistint_t));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	p->n = n;
+	p->prev = NULL;
+
+	if (*head == NULL)
+	{
+		*head = p;
+	}
+	else
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+	}
+	tmp->next = p;
+	p->prev = tmp;
+	return (p);
+}
