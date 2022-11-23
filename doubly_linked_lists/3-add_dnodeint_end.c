efa7e0bba731a4301cee9e7fbb206fd1ec1712b1
Valentin <5619@holbertonstudents.com>
2022-11-23 05:34:48 -0800

    3.add node addd the end of a double ll

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,6 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *p, *tmp;
 
+	tmp = *head;
 	p = malloc(sizeof(dlistint_t));
 	if (p == NULL)
 	{
@@ -22,14 +23,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	p->n = n;
 	p->next = NULL;
-	tmp = *head;
 
 	if (*head == NULL)
 	{
 		p->prev = NULL;
 		*head = p;
 	}
-
 	while (tmp->next != NULL)
 	{
 		tmp = tmp->next;
