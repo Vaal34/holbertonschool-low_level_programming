0633fcc0f2d9fa41e63ad28afbdd4ab7fefde4d8
Valentin <5619@holbertonstudents.com>
2022-11-23 05:51:25 -0800

    4.free double list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -8,11 +8,8 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	if (head == NULL)
-		free(head);
-	else
+	if (head != NULL)
 	{
-		free_dlistint(head->prev);
 		free_dlistint(head->next);
 		free(head);
 	}
