832d53f6759489da699c5d1e90e7496037b32afa
Valentin <5619@holbertonstudents.com>
2023-01-05 05:38:19 -0800

    task 1

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "search_algos.h"
+#include <math.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * binary_search - binary search algo
+ * @array: pointer of array
+ * @size: size of array
+ * @value: value of element of array
+ * Return: i or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	size_t l = 0;
+	size_t h = size - 1;
+	size_t cut;
+
+	printf("Searching in array: ");
+	for (i = 0; i < h; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[h]);
+
+	while (l < h)
+	{
+		cut = floor((l + h) / 2);
+
+		if (value > array[cut])
+		{
+			l = cut + 1;
+			printf("Searching in array: ");
+			for (i = l; i < h; i++)
+				printf("%d, ", array[i]);
+			printf("%d\n", array[h]);
+			continue;
+		}
+		else if (value < array[cut])
+		{
+			h = cut - 1;
+			printf("Searching in array: ");
+			for (i = l; i < h; i++)
+				printf("%d, ", array[i]);
+			printf("%d\n", array[h]);
+			return (i);
+		}
+		else
+			return (cut);
+	}
+	return (-1);
+}
