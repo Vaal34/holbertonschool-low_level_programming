fba171339f753b4ed08dc5c28e7b0ae4f49303b1
Valentin <5619@holbertonstudents.com>
2023-01-05 01:32:05 -0800

    fix task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,11 +14,16 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
+	if (value <= 0)
+		return (-1);
+
 	for (i = 0; i < size - 1; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			break;
+		if (array[i] <= 0)
+			return (-1);
 	}
 	return (i);
 }
